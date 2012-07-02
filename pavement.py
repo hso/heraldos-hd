ca8df31862c7c1483943dcc3a584c8e230d49c29
José Sazo <jose.sazo@gmail.com>
2012-07-02 16:51:15 -0400

    Add 'build' task. Move source files into 'src'.
    Now you can run
    
    $ python setup.py build
    
    and watch as the generated pdfs and midis begin to appear into 'output'.

--- pavement.py
+++ pavement.py
@@ -3,4 +3,24 @@ from paver.easy import *
 @task
 def build():
     """Build everything"""
-    print "Hello"
+    songs = path('./src').dirs()
+    outputdir = path('./output')
+    outputdir.rmtree()
+    outputdir.mkdir()
+
+    for song in songs:
+        ly = song / 'main.ly'
+        output = outputdir / song.basename()
+        sh("lilypond -o %s -I %s %s" % (output.abspath(), song.abspath(), ly.abspath()))
+    
+@task
+def clean():
+    """Remove generated files (PDFs, MIDIs, etc)"""
+    
+    songs = path('./src').dirs()
+    for song in songs:
+        files = song.files('*.midi')
+        for file in files:
+            file.remove()
+    print 'Removed'
+    
