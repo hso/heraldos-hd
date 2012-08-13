8ca2a8c0e019fe8595ef4a2ed986f557a66f712d
José Sazo <jose.sazo@gmail.com>
2012-08-12 23:39:13 -0400

    Add option to build single songs

--- pavement.py
+++ pavement.py
@@ -1,15 +1,31 @@
 from paver.easy import *
 
-@task
-def build():
-    """Build everything"""
-    songs = path('./src').dirs()
-    outputdir = path('./output')
-    outputdir.rmtree()
-    outputdir.mkdir()
+SOURCE_DIR = path('./src')
+SONGS = SOURCE_DIR.dirs()
+OUTPUT_DIR = path('./output')
+MAIN = 'main.ly'
 
-    for song in songs:
-        ly = song / 'main.ly'
-        output = outputdir / song.basename()
-        sh("lilypond -o %s -I %s %s" % (output.abspath(), song.abspath(), ly.abspath()))
-    
+@task
+@cmdopts([
+        ('all', 'a', 'Build everything'),
+        ('song=', 's', 'Song to bunild')
+])
+def build(options):
+    """Build Lilypond source files"""
+    if options.build.has_key('all'):
+        for song_path in SONGS:
+            process(song_path)
+    elif options.build.has_key('song'):
+        p = SOURCE_DIR / options.build.song
+        if p.exists():
+            process(p)
+        else:
+            print "No such thing"
+    else:
+        print 'To build everything, use -a. Specify a song to build with -s <SONG>'
+
+def process(song_path):
+    song_name = song_path.basename()
+    ly = song_path / MAIN
+    output = OUTPUT_DIR / song_name
+    sh("lilypond -o %s -I %s %s" % (output.abspath(), song_path.abspath(), ly.abspath()))
